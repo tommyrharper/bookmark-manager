7e9228bfe3d5c70d7c474d40e1ef71a0b690ad8f
tommyrharper <tommyrharper@gmail.com>
2020-04-07 19:01:12 +0100

    test failed - create bookmark function failed

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -15,4 +15,12 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.google.com")
     end
   end
+
+  describe './create' do
+    it 'creates a new bookamrk' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
+  end
 end
