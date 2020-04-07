dbea63a101aba71c12798c6f5567252eaf292a91
tommyrharper <tommyrharper@gmail.com>
2020-04-07 12:34:57 +0100

    tests failed - incorrect content on bookmarks page

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -9,4 +9,18 @@ feature 'You can view bookmarks' do
     expect(page).to have_content 'Google'
   end
 
+  scenario 'the page shows the second bookmark name' do
+    visit '/bookmarks'
+    expect(page).to have_content 'BBC'
+  end
+
+  scenario 'the page shows the first bookmark url' do
+    visit '/bookmarks'
+    expect(page).to have_content 'www.google.com'
+  end
+
+  scenario 'the page shows the second bookmark url' do
+    visit '/bookmarks'
+    expect(page).to have_content 'www.bbc.co.uk'
+  end
 end
