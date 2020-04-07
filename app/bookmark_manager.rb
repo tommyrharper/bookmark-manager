05676913747da0cd0d4366437d280118416a80c2
tommyrharper <tommyrharper@gmail.com>
2020-04-07 12:47:02 +0100

    code refactored to use MVC

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
@@ -7,6 +8,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
+    @bookmarks = Bookmark.all
     erb :bookmarks
   end
 
