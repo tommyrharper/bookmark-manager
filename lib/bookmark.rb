04d829ea65038e90f9644eeddd2329528ec727d3
tommyrharper <tommyrharper@gmail.com>
2020-04-07 15:31:14 +0100

    bookmark.rb refactored to use the database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.google.com"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 end
