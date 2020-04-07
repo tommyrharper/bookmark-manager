7c3b91d8c6d9fccdcac572cf16e08fb3b5485077
tommyrharper <tommyrharper@gmail.com>
2020-04-07 12:33:16 +0100

    tests failed - bookmarks page does not display correct information

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require_relative './bookmark_manager/bookmark_manager.rb'
+require_relative './app/bookmark_manager.rb'
 run BookmarkManager
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -4,4 +4,9 @@ feature 'You can view bookmarks' do
     expect(page).to have_content 'Bookmarks'
   end
 
+  scenario 'the page shows the first bookmark name' do
+    visit '/bookmarks'
+    expect(page).to have_content 'Google'
+  end
+
 end
